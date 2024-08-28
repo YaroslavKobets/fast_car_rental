a9b9e539dc40dddffdc651d982022880086ebdf9
Yaroslav Kobets <yaroslavkobetsV@gmail.com>
2024-08-28 11:47:00 +0300

    fix: add handleScroll fn

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,11 +5,17 @@ import Image from 'next/image'
 import CustomButton from './CustomButton'
 
 const Hero = () => {
-	const handleScroll = () => {}
+	const handleScroll = () => {
+		const nextSection = document.getElementById('discover')
+
+		if (nextSection) {
+			nextSection.scrollIntoView({ behavior: 'smooth' })
+		}
+	}
 
 	return (
 		<div className='hero'>
-			<div className='flex-1 pt-36 pl-4'>
+			<div className='flex-1 pt-36 sm:pl-16 pl-6'>
 				<h1 className='hero__title'>
 					Find, book, or rent a car - quickly and easily!
 				</h1>
